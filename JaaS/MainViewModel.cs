68497bfc6cbb9feb9a5e53a1bc90613bdd92a8c1
John Staveley <john@staveley.org>
2023-11-13 07:18:36 +0000

    speak responses to interactions

--- JaaS/MainViewModel.cs
+++ JaaS/MainViewModel.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace JaaS;
 
 public class MainViewModel : ViewModelBase
 {
     private SpeechRecognitionEngine _recognizer;
+    private SpeechSynthesizer _synthesizer;
 
     private string? _recognizedText;
-    public string? RecognizedText
+    public string? ResponseText
     {
         get => _recognizedText;
         set
@@ -21,8 +24,10 @@ public class MainViewModel : ViewModelBase
     public MainViewModel()
     {
         _recognizer = new SpeechRecognitionEngine();
+        _synthesizer = new SpeechSynthesizer();
         _recognizedText = string.Empty;
-        InitializeRecognitionEngine();
+        InitialiseRecognitionEngine();
+        InitialiseSpeechSynthesiserEngine();
     }
 
     public void Dispose()
@@ -30,8 +35,12 @@ public class MainViewModel : ViewModelBase
         if (_recognizer != null)
             _recognizer.Dispose();
     }
+    private void InitialiseSpeechSynthesiserEngine()
+    {
+        _synthesizer.SetOutputToDefaultAudioDevice(); // Set output to speakers
+    }
 
-    private void InitializeRecognitionEngine()
+    private void InitialiseRecognitionEngine()
     {
         _recognizer.SetInputToDefaultAudioDevice();
         GrammarBuilder builder = new GrammarBuilder();
@@ -55,45 +64,51 @@ public class MainViewModel : ViewModelBase
         _recognizer.BabbleTimeout = TimeSpan.FromSeconds(3);
         _recognizer.InitialSilenceTimeout = TimeSpan.FromSeconds(3);
         _recognizer.RecognizeCompleted += RecognizeCompleted;
+
     }
 
     private void RecognizeCompleted(object? sender, RecognizeCompletedEventArgs e)
     {
         if (e.Result != null)
         {
+            var responseText = "";
+            var speakResult = true;
             switch (e.Result.Text)
             {
                 case "Hello":
-                    RecognizedText = "Hello, World!";
+                    responseText = "Hello, World!";
                     break;
                 case "Jars":
-                    RecognizedText = "Jars is a great guy!";
+                    responseText = "Jars is a great guy!";
                     break;
                 case "Close":
-                    RecognizedText = "Closing the application.";
+                    responseText = "Closing the application.";
                     Close();
                     break;
                 case "sponsor":
-                    RecognizedText = "This episode is sponsored by JetBrains.";
+                    responseText = "This meetup is sponsored by Fruition IT, Bruntwood and JetBrains.";
                     break;
                 case "next event":
-                    RecognizedText = "The next event is on the 20th of April.";
+                    responseText = "The next event is on the 25th of Jaunary by Michael Gray talking about what is the role of a principal engineer.";
                     break;
                 case "Open the pod bay doors":
-                    RecognizedText = "I'm sorry, Dave. I'm afraid I can't do that.";
+                    responseText = "I'm sorry Dave. I'm afraid I can't do that.";
                     break;
                 default:
-                    RecognizedText = e.Result.Text;
+                    responseText = e.Result.Text;
+                    speakResult = false;
                     break;
             }
+            if (speakResult && e.Result.Confidence > 0.7) _synthesizer.SpeakAsync(responseText);
+            ResponseText = responseText;
         }
         else
-            RecognizedText = "I have no idea what you just said.";
+            ResponseText = "I have no idea what you just said.";
     }
 
     public void ActivateRecognition()
     {
-        RecognizedText = string.Empty;
+        ResponseText = string.Empty;
         _recognizer.RecognizeAsync();
     }
     public event Action RequestClose;
