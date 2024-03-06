dffd15a3cb8d87be0519710dafdea30e8b916dfc
John Staveley <john@staveley.org>
2024-03-06 06:27:23 +0000

    Demo 5

--- JaaS.Tests/D5_AzureSpeechRecogniserDemo.cs
+++ JaaS.Tests/D5_AzureSpeechRecogniserDemo.cs
@@ -1,3 +1,4 @@
+using JaaS.Demos.Utility;
 using JaaS.Models;
 using Microsoft.CognitiveServices.Speech;
 using Microsoft.CognitiveServices.Speech.Audio;
@@ -16,15 +17,18 @@ public class D5_AzureSpeechRecogniserDemo
         _configuration = new AppConfiguration();
         azureSpeechConfig = SpeechConfig.FromSubscription(_configuration.AzureSpeechSubscriptionKey, _configuration.AzureSpeechRegion);
         azureSpeechConfig.SpeechRecognitionLanguage = "en-GB";
-        _speechRecognizerAzure = new SpeechRecognizer(azureSpeechConfig, AudioConfig.FromDefaultMicrophoneInput());
-
     }
 
     [Test]
-    [TestCase("Hello")]
-    public async Task RecogniseVoice(string expectedWord)
+    [TestCase("Speakers", "speakers")]
+    [TestCase("WhoAreTheSpeakers", "who are the speakers this week?")]
+    public async Task RecogniseVoice(string file, string expectedWord)
     {
         // Arrange
+        var bytes = FileUtility.LoadBytesFromEmbeddedResource($"JaaS.Demos.Resources.{file}.wav");
+        var audioInputStream = AudioInputStream.CreatePushStream(AudioStreamFormat.GetWaveFormatPCM(44100, 16, 1));
+        audioInputStream.Write(bytes);
+        _speechRecognizerAzure = new SpeechRecognizer(azureSpeechConfig, AudioConfig.FromStreamInput(audioInputStream));
         Assert.IsNotNull(_speechRecognizerAzure);
         var response = "";
 
--- JaaS.Tests/Utility/FileUtility.cs
+++ JaaS.Tests/Utility/FileUtility.cs
@@ -12,4 +12,17 @@ public static class FileUtility
         stream.Seek(0, SeekOrigin.Begin);
         return stream;
     }
+    public static byte[] LoadBytesFromEmbeddedResource(string resource)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var stream = assembly.GetManifestResourceStream(resource);
+        Assert.IsNotNull(stream);
+        stream.Seek(0, SeekOrigin.Begin);
+        using (MemoryStream ms = new MemoryStream())
+        {
+            stream.CopyTo(ms);
+            return ms.ToArray();
+        }
+    }
+
 }
